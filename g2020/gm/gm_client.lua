84ff5184031d4d67aeaa67aebe3ec50e8a34ad8a
leiyonghui <729186631@qq.com>
2019-11-29 18:50:31 +0800

    商店GM

--- g2020/gm/gm_client.lua
+++ g2020/gm/gm_client.lua
@@ -4,4 +4,13 @@ local GMItem = GM:createGMItem()
 GMItem["g2020/好友界面"] = function()
     Lib.emitEvent(Event.EVENT_SHOW_FRIEND, true)
 end
+
+GMItem["g2020/特权商店"] = function()
+    Lib.emitEvent(Event.EVENT_SHOW_PRI_SHOP, true)
+end
+
+GMItem["g2020/金币商店"] = function()
+    Lib.emitEvent(Event.EVENT_SHOW_GOLD_SHOP, true)
+end
+
 return GMItem
