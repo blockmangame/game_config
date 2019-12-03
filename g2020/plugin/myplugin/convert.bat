4343ba480018ee19ab933cee5ec88133949e69f7
snowdoufu <495460441@qq.com>
2019-12-03 10:21:52 +0800

    第3个房子

--- g2020/plugin/myplugin/convert.bat
+++ g2020/plugin/myplugin/convert.bat
@@ -1,3 +1,3 @@
 @rem ..\..\..\..\..\dev\tools\lualibs\lua\lua53.exe ..\..\..\..\tools\json2csv.lua %1
-@..\..\..\..\..\dev\tools\lualibs\lua\lua53.exe ..\..\..\..\tools\JsonCsvConverter.lua %1
+@D:\git\dev\blockmango-engine2\dev\tools\lualibs\lua\lua53.exe D:\git\dev\blockmango-engine2\res\tools\JsonCsvConverter.lua %1
 @pause
