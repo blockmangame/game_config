7bcd2e081b8f59db45fe6f31e2e83bf19d1b5e8e
yaocanjie <1184419984@qq.com>
2020-02-05 19:39:34 +0800

    player_packet配置提交

---
+++ g2020/script_server/player_packet.lua
@@ -0,0 +1,7 @@
+local setting = require "common.setting"
+local handles = Player.PackageHandlers
+
+function handles:UseItemEquipSkill(packet)
+	local item = Item.CreateSlotItem(self, packet.tid, packet.slot)
+	Trigger.CheckTriggers(item:cfg(), "USE_ITEM", {obj1 = self, itemName = item:cfg().fullName})
+end
