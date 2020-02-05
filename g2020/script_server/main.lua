e551a83aa8a2cd587191db29af0add75a1d05bea
yaocanjie <1184419984@qq.com>
2020-02-05 17:20:04 +0800

    提交骑乘道具配置

--- g2020/script_server/main.lua
+++ g2020/script_server/main.lua
@@ -1,2 +1,3 @@
 
 require "script_server.script_actions"
+require "script_server.player_packet"
--- g2020/script_server/script_actions.lua
+++ g2020/script_server/script_actions.lua
@@ -12,3 +12,39 @@ function Actions.ShowHomeGuide(data, params, context)
 		pos = pos
 	})
 end
+
+function Actions.EquipItem(data, params, content)
+	local item = params.item
+	local entity = params.entity
+	if not item or not entity then
+        return 
+    end
+    local curTid = item:tid()
+	local curSlot = item:slot()
+    local types = item:tray_type()
+	local dstTid, dstSlot
+    for tid, _ in pairs(types) do
+        local ret = entity:tray():query_trays(tid)
+        for _, element in pairs(ret) do
+            local _tid, _tray = element.tid, element.tray
+            local slot = _tray:find_free()
+            if curTid ~= _tid then
+				dstTid = _tid
+				dstSlot = slot or 1
+            end
+        end
+    end
+	if not dstTid then
+		return
+	end
+	local entityTrays = entity:tray()
+	local tray_1 = entityTrays:fetch_tray(curTid)
+	local tray_2 = entityTrays:fetch_tray(dstTid)
+	
+	if not Tray:check_switch(tray_1, curSlot, tray_2, dstSlot) then
+		return
+	end
+
+	Tray:switch(tray_1, curSlot, tray_2, dstSlot) 
+	entity:syncSkillMap()
+end
