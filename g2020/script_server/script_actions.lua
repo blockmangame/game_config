6b78096bdcdc18dec76067617f1576bf5198dd51
luoweishuan <vin_120@163.com>
2020-02-14 15:50:47 +0800

    删除没用到的方法

--- g2020/script_server/script_actions.lua
+++ g2020/script_server/script_actions.lua
@@ -1,22 +1,6 @@
 local BehaviorTree = require("common.behaviortree")
 local Actions = BehaviorTree.Actions
 
-function Actions.RefreshNumberBoardText(data, params, context)
-	params.player:sendPacket({
-		pid = "RefreshNumberBoardText",
-		wndKey = params.wndKey,
-		text = params.text,
-	})
-end
-
-function Actions.RefreshBlackBoardText(data, params, context)
-	params.player:sendPacket({
-		pid = "RefreshBlackBoardText",
-		wndKey = params.wndKey,
-		text = params.text,
-	})
-end
-
 function Actions.ShowHomeGuide(data, params, context)
 	local entity = params.entity
 	local pos = entity:getPosition()
