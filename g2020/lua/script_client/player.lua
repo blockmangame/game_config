6574d1f87e0b636aa2d2b58727493df25d7d69ad
yaocanjie <1184419984@qq.com>
2020-02-26 14:00:39 +0800

    修复背包问题

--- g2020/lua/script_client/player.lua
+++ g2020/lua/script_client/player.lua
@@ -1,6 +1,21 @@
 local setting = require "common.setting"
 
 function Player:setItemUse(tid, slot, isUse, disSendSer)
+	local function updateHandItem(self, tid, slot)
+		local handItem = self:data("main").handItem
+		if not handItem or handItem:null() then
+			self:data("main").handItem = nil
+		end 
+		local item = Item.CreateSlotItem(self, tid, slot)
+		if isUse and item and not item:null() then
+			local buffName = item:cfg().equip_buff
+			if buffName then
+				self:data("main").handItem = item
+			end
+		end
+		Lib.emitEvent(Event.EVENT_HAND_ITEM_CHANGE)
+	end
+
     if not tid or not slot then
         return
     end
@@ -9,18 +24,17 @@ function Player:setItemUse(tid, slot, isUse, disSendSer)
         useItemList = {}
         self:data("main").useItemList = useItemList
     end
-    local tidUseItemList = useItemList[tid]
+	local tidUseItemList = useItemList[tid]
     if tidUseItemList and tidUseItemList[slot] == isUse then
-        if disSendSer then
-            Lib.emitEvent(Event.EVENT_HAND_ITEM_CHANGE)
+		if disSendSer then
+			updateHandItem(self, tid, slot)
         end
         return 
     end
     useItemList[tid] = useItemList[tid] or {}
     useItemList[tid][slot] = isUse
-
-    if disSendSer then
-        Lib.emitEvent(Event.EVENT_HAND_ITEM_CHANGE)
+	if disSendSer then
+		updateHandItem(self, tid, slot)
         return
     end
     local packet = {
