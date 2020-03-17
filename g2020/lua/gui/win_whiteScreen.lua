fc5fb15c127cdd66ff544ead4b477eddfd13dd20
luoweishuan <vin_120@163.com>
2020-03-17 11:35:03 +0800

    白光渐变

--- g2020/lua/gui/win_whiteScreen.lua
+++ g2020/lua/gui/win_whiteScreen.lua
@@ -1,13 +1,28 @@
 function M:init()
     WinBase.init(self, "WhiteScreen.json", true)
+    self.colorSign = 1
     self:initEvent()
+end
+
+local function setWhiteScreen(self)
+    if self.colorSign <= 0 then
+        return
+    end
+
+    self.colorTimer = World.Timer(5, function()
+        self.colorSign = self.colorSign - 0.5
+        self._root:SetBackgroundColor({1, 1, 1, self.colorSign})
+        setWhiteScreen(self)
+    end)
 end
 
 function M:initEvent()
     Lib.subscribeEvent(Event.EVENT_SHOW_WHITE_SCREEN, function(args)
         self._root:SetBackgroundColor({1, 1, 1, 1})
+        self.colorSign = 1
+
         World.Timer(60, function()
-            self._root:SetBackgroundColor({1, 1, 1, 0})
+            setWhiteScreen(self)
         end)
     end)
 end
