1e8a2be5b153d4f456b93408126394859c831248
HeShenghuan <heshfree@hotmail.com>
2020-03-31 15:54:49 +0800

    式神相关基本测试

--- g2030/lua/gm_server.lua
+++ g2030/lua/gm_server.lua
@@ -10,5 +10,28 @@ end
 GMItem["g2030/addBuff"] = function(self)
     self:addBuff("myplugin/example", -1)
 end
+-----------------------------------Pet Model Test----------------------------------
+local Entity
+GMItem["sample/创建一个跟随宠物"] = function(self)
+    local entity = EntityServer.Create({cfgName = "myplugin/pet_1_1_1", pos = self:getPosition()})
+    --table.insert(EntityList, entity)
+    Entity = entity
+    local control = entity:getAIControl()
+    control:setFollowTarget(self)
+end
 
+GMItem["sample/一个抽奖点"] = function(self)
+    local entity = EntityServer.Create({cfgName = "myplugin/roller1", pos = self:getPosition()})
+    table.insert(EntityList, entity)
+end
+
+GMItem["sample/删除上一个宠物"] = function(self)
+    Entity:destroy()
+    --table.remove(EntityList,#EntityList)
+end
+
+GMItem["sample/释放宠物技能"] = function(self)
+    Skill.Cast("myplugin/pet_1_1_1_attack", {targetID=self.objID}, Entity)
+end
+-----------------------------------Pet Model Test End-------------------------------
 return GMItem
