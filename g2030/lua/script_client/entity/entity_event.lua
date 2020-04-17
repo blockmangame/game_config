7f2e479e37e22343d61ddeee4409881b958ee488
wangpiqi <737711464@qq.com>
2020-04-17 16:17:43 +0800

    玩家进出方块区域事件

--- g2030/lua/script_client/entity/entity_event.lua
+++ g2030/lua/script_client/entity/entity_event.lua
@@ -11,9 +11,34 @@ function entity_event(entity, event, ...)
 end
 
 function events:onBlockChanged(oldId, newId)
-    print("onBlockChanged ", oldId, newId)
+    --print("onBlockChanged ", oldId, newId)
 end
 
 function events:inBlockChanged(oldId, newId)
     print("inBlockChanged ", oldId, newId)
+
+    if not self.isPlayer then
+        return
+    end
+
+    if self.objID ~= Me.objID then
+        return
+    end
+
+    if oldId == newId then
+        return
+    end
+
+    local RegionBlock = require "script_client.world.region.region_block"
+
+    local RegionConfig = T(Config, "RegionConfig") ---@type RegionConfig
+    local leaveRegionConfig = RegionConfig:getRegionConfigByBlockId(oldId)
+    if leaveRegionConfig then
+        RegionBlock:onEntityLeave(self, leaveRegionConfig)
+    end
+
+    local enterRegionConfig = RegionConfig:getRegionConfigByBlockId(newId)
+    if enterRegionConfig then
+        RegionBlock:onEntityEnter(self, enterRegionConfig)
+    end
 end
