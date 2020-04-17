4386a62ecf08fc827d175c7fadb0c1aca55240e0
wangpiqi <737711464@qq.com>
2020-04-17 16:51:56 +0800

    玩家进入区域事件，服务器

---
+++ g2030/lua/script_server/entity/entity_event.lua
@@ -0,0 +1,23 @@
+local entityEventEngineHandler = L("entityEventEngineHandler", entity_event)
+
+local events = {}
+
+function entity_event(entity, event, ...)
+    entityEventEngineHandler(entity, event, ...)
+    local func = events[event]
+    if func then
+        func(entity, ...)
+    end
+end
+
+function events:onBlockChanged(oldId, newId)
+    --print("onBlockChanged ", oldId, newId)
+end
+
+function events:inBlockChanged(oldId, newId)
+    print("inBlockChanged ", oldId, newId)
+
+    if not self.isPlayer then
+        return
+    end
+end
